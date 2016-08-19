ece9d62228077ecb889468224b2db829c3200ea8
Eduardo Jorge <herulume@users.noreply.github.com>
2016-08-19 15:41:49 +0100

    Update timeline.py

--- timeline.py
+++ timeline.py
@@ -1,33 +1,53 @@
-from twitter import *
+#!/usr/bin/python3
+from twython import Twython, TwythonError
 import os
 import argparse
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-t', '--target', required=True, help='Their @')
 args = vars(ap.parse_args())
-alvo = args["target"]
+target = args["target"]
 
-con_secret = ''
-con_secret_key = ''
-token = ''
-token_key = ''
+APP_KEY = ""
+APP_SECRET = ""
+OAUTH_TOKEN = ""
+OAUTH_TOKEN_SECRET = ""
 
-#auth
-t = Twitter(auth=OAuth(token, token_key, con_secret, con_secret_key), retry=True) 
+t = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 def timeline(x):
-#get the timeline
-	timeline = t.statuses.user_timeline(screen_name=x, include_rts=False, count=200) 
+	try:
+		timeline = t.get_user_timeline(screen_name=x)
 
-	for tweet in timeline: #for each tweet
-		for i in range(len(tweet['entities']['urls'])): #check how many links per tweet and iterate over them
-			link = tweet['entities']['urls'][i]['expanded_url'] #saves the link for future use
-			if ('facebook' in link) or ('instagram' in link) or ('tumblr' in link): 
-				print "[*] Potencial profile found: %s" % (link)
+		for tweet in timeline: #for each tweet
+			for i in range(len(tweet['entities']['urls'])): #check how many links per tweet and iterate over them
+				link = tweet['entities']['urls'][i]['expanded_url'] #saves the link for future use
+				if ('facebook' in link) or ('instagram' in link) or ('tumblr' in link): 
+					print ("[*] Potencial profile found: %s" % (link))
+
+	except TwythonError as e:
+		print(e)
+
+def bio(x):
+	try:
+		info = t.show_user(screen_name=x)
+
+		bio = info['description']
+		if ('facebook' in bio) or ('instagram' in bio) or ('tumblr' in bio) or ('snap' in bio):
+			print ("[*] Potencial profile found in bio: %s" % (link))
+
+		if ('url' in bio):
+			link = info['entities']['url']['urls'][0]['expanded_url']
+			if ('facebook' in link) or ('instagram' in link) or ('tumblr' in link):
+				print ("[*] Potencial profile found in bio's URL: %s" % (link))
+	except TwythonError as e:
+		print(e)
 
 def main():
-	os.system('clear')
+	os.system("clear")
+	print("Looking for links...")
 	timeline(target)
+	bio(target)
 
 if __name__ == '__main__':
-	main()
+	main() 
